05afffea4f90d2fd33203d8b55f5b9911e07a3bf
PranayPant <ppantg1@gmail.com>
2015-05-10 11:01:23 -0700

    Fixed redirect issues with Google OAuth2.

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,13 +1,32 @@
 class CallbacksController < Devise::OmniauthCallbacksController
-    
+
     def facebook
-        @user = User.from_omniauth(request.env["omniauth.auth"])
-      	sign_in_and_redirect(@user)
+        do_callbacks
     end
 
     def google_oauth2
+    	do_callbacks
+    end
+
+    private
+
+    def do_callbacks
+
     	@user = User.from_omniauth(request.env["omniauth.auth"])
-      	sign_in_and_redirect(@user)
+
+        if @user.persisted?
+      		sign_in_and_redirect(@user)
+      	else
+      		byebug
+      		provider = @user.provider
+      		uid = @user.uid
+      		@user = User.find_by_email(@user.email)
+      		@user.provider = provider
+      		@user.uid = uid
+      		@user.save!
+      		session["devise.user.attributes"] = @user.attributes
+      		sign_in_and_redirect(@user)
+      	end
     end
     
 end
