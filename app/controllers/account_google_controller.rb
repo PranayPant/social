ab2eb88d1329d12bef33690f4103e97fc96c22fd
PranayPant <ppantg1@gmail.com>
2015-06-11 16:12:50 -0700

    prelim

--- app/controllers/account_google_controller.rb
+++ app/controllers/account_google_controller.rb
@@ -1,6 +1,13 @@
 
 class GoogleFeaturesController < ApplicationController
 
+  require 'net/http'
+  require 'google/api_client'
+
+  @@scopes = {'gmail' => 'https://mail.google.com/'}
+
+  def authorize
+  end
 	
 
 end
