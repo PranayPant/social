983a065dcd71060cbf49a02f1eb20d645f1c193f
PranayPant <ppantg1@gmail.com>
2015-06-11 17:23:06 -0700

    prelim

--- app/controllers/account_google_controller.rb
+++ app/controllers/account_google_controller.rb
@@ -15,6 +15,17 @@ class GoogleFeaturesController < ApplicationController
 
     scope = @@scopes[feature]
 
+    auth_client.update!(scope: scope)
+    auth_uri = auth_client.authorization_uri
+
+    # Redirect to auth_uri and get code
+    redirect_to (auth_uri)
+    res = Net::HTTP.get_response(URI(auth_uri))
+
+    auth_client.code = res.code
+
+
+
 
   end
 	
