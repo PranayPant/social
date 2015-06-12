85d712897a96cc6f5c940c9b9ba9b0f55818f5d5
PranayPant <ppantg1@gmail.com>
2015-06-12 10:08:34 -0700

    'prelim'

--- app/controllers/account_google_controller.rb
+++ app/controllers/account_google_controller.rb
@@ -14,18 +14,7 @@ class GoogleFeaturesController < ApplicationController
     auth_client = client_secrets.to_authorization
 
     scope = @@scopes[feature]
-    redirect_uri = 'http://www.my-social-hub.herokuapp.com/oauth2callback'
-
-    auth_client.update!(
-      :scope => 'https://www.googleapis.com/auth/drive.metadata.readonly',
-      :redirect_uri => 'https://www.google.com'
-    )
-    auth_uri = auth_client.authorization_uri
-
-    # Redirect to auth_uri and get code
-    redirect_to (auth_uri)
-    res = Net::HTTP.get_response(URI(auth_uri))
-    auth_client.code = res.code
+    
 
   end
 	
