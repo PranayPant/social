b33c187835a41b7184829221ab7e8e7f13465179
PranayPant <ppantg1@gmail.com>
2015-06-11 23:16:59 -0700

    'prelim'

--- app/controllers/account_google_controller.rb
+++ app/controllers/account_google_controller.rb
@@ -14,7 +14,16 @@ class GoogleFeaturesController < ApplicationController
     auth_client = client_secrets.to_authorization
 
     scope = @@scopes[feature]
-   
+    redirect_uri = 'http://www.my-social-hub.herokuapp.com/oauth2callback'
+
+    auth_client.update!(scope: scope, redirect_uri: redirect_uri)
+    auth_uri = auth_client.authorization_uri
+
+    # Redirect to auth_uri and get code
+    redirect_to (auth_uri)
+    res = Net::HTTP.get_response(URI(auth_uri))
+    auth_client.code = res.code
+
   end
 	
 
